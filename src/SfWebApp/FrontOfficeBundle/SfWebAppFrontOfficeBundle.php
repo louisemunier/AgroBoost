bc90c34fe120aed1bcae0b8719b4b2831b9bd2c1
Messant <messant.d@gmail.com>
2016-11-16 11:36:57 +0100

    ajout Bundles

---
+++ src/SfWebApp/FrontOfficeBundle/SfWebAppFrontOfficeBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SfWebApp\FrontOfficeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SfWebAppFrontOfficeBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
