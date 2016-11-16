19a96412500a077384ac21d271a0ba5545e71375
louisemunier <munier.louise0@gmail.com>
2016-11-16 23:49:10 +0100

    Added page fiche

---
+++ src/SfWebApp/CmsBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SfWebApp\CmsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SfWebAppCmsBundle:Default:index.html.twig');
+    }
+
+    public function ficheAction()
+    {
+        return $this->render('SfWebAppCmsBundle:Default:fiche.html.twig');
+    }
+}
