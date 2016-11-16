60991ab87d95028febbe2296be96908ac14b7dfb
Messant <messant.d@gmail.com>
2016-11-16 13:01:55 +0100

    ajout dossier css

--- src/SfWebApp/BackOfficeBundle/Controller/DefaultController.php
+++ src/SfWebApp/BackOfficeBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SfWebAppBackOfficeBundle:Default:index.html.twig');
+        return $this->render('SfWebAppBackOfficeBundle:Default:login.html.twig');
     }
 }
